ee458280fe29388f40b6a5c4e88f7dcc0a756a52
Andre Scalabrini <andrescalabr@gmail.com>
2023-05-15 16:18:09 -0300

    Atualização dia 15/05/23

--- 150523 nextion archive and arduino code/AD2_v206_Nextion/transferenciaAbas.ino
+++ 150523 nextion archive and arduino code/AD2_v206_Nextion/transferenciaAbas.ino
@@ -30,3 +30,84 @@ void setinhasnivel(){
   //printa as setinhas do nível
 }
 
+void configangulo(){
+  //só segue o nome
+}
+
+void configdefabrica(){
+  //só segue o nome
+}
+
+void alteracaodelado(){
+  //só segue o nome
+}
+
+void pagcamber(){
+  //só segue o nome
+}
+
+void pagsalvar(){
+  //só segue o nome
+}
+
+void pagcaster(){
+  //só segue o nome
+}
+
+void pagzerar(){
+  //só segue o nome
+}
+
+void saindodomenu(){
+  //só segue o nome
+}
+
+void saindodoajuste(){
+  //só segue o nome
+}
+
+void laserligado(){
+  //só segue o nome
+}
+
+void laserdesligado(){
+  //só segue o nome
+}
+
+void parametrodabateria8v(){
+  //sei la oq isso significa mas tamo nois
+}
+
+void inicial(){
+  //só segue o nome
+}
+
+void textoinicial(){
+  //texto 1
+  //texto 2
+  //texto 3 (1 e 2 juntos)
+}
+
+void iniciopadraoslx(){
+  //só segue o nome
+}
+
+void tecladoconfig(){
+  //só segue o nome
+}
+
+void sensibilidadeconfig(){
+  //só segue o nome
+}
+
+void calculodekpiconfig(){
+  //só segue o nome
+}
+
+void configdeangulodegiro(){
+  //só segue o nome
+}
+
+void configlado(){
+  //só segue o nome
+}
