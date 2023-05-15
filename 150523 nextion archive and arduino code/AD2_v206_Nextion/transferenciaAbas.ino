9dc3c5f8937cfbc54c8702e10b7d4a1b8905c5a4
Andre Scalabrini <andrescalabr@gmail.com>
2023-05-15 15:53:30 -0300

    Atualização dia 15/05/23

---
+++ 150523 nextion archive and arduino code/AD2_v206_Nextion/transferenciaAbas.ino
@@ -0,0 +1,32 @@
+void bateriacarregando(){
+  //raiozinho na frente da bateria
+}
+
+void sembateria(){
+  //como o nome já diz, o aparelho ta sem carga
+}
+
+void niveldabateria(){
+  //mexer a barrinha de acordo com a porcentagem
+}
+
+void simbolowifi(){
+  //vai mostrar o simbolo do wifi ou não
+}
+
+void bolhanivel(){
+  //localização e design bolha nivel
+}
+
+void textoinicializacao(){
+  // printa o texto de inicialização no display
+}
+
+void printagraus(){
+  //printa no display os graus
+}
+
+void setinhasnivel(){
+  //printa as setinhas do nível
+}
+
